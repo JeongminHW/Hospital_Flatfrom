cd4e9f30f39adca6f11e168bd9f0b41d16669b6e
DESKTOP-9VIM0DL\yebin <yebin1801@naver.com>
2023-12-08 14:01:05 +0900

    데이터베이스프로그래밍 프로젝트

---
+++ java/DB-Project/src/Doctor/DrAnswer.java
@@ -0,0 +1,17 @@
+package Doctor;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import DB.*;
+public class DrAnswer extends JFrame {
+
+	public DrAnswer() {
+		
+	}
+	
+    public static void main(String[] args) {
+        new DrAnswer();
+    }
+}
---
+++ java/DB-Project/src/Doctor/DrMain.java
@@ -0,0 +1,13 @@
+package Doctor;
+
+import User.*;
+
+import DB.*;
+
+public class DrMain {
+
+
+    public static void main(String[] args) {
+        new DrMain();
+    }
+}
